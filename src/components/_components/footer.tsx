494877f09e4831a4bde02aa8ad787dec084438dd
Dylan Chan <chan0566@algonquinlive.com>
2026-05-20 11:26:54 -0400

    fix: footer stylling

--- src/components/_components/footer.tsx
+++ src/components/_components/footer.tsx
@@ -6,30 +6,32 @@ import LinkedinIcon from "../icons/linkedin-icon";
 
 export default function Footer() {
   return (
-    <header className="flex items-center justify-center gap-8 py-2 px-8 w-full bg-background/40">
-      <Link
-        href="https://www.linkedin.com/in/dylan-chan-a92b07271/"
-        target="blank"
-        className="flex gap-1 items-center no-underline"
-      >
-        <span>LinkedIn</span>
-        <LinkedinIcon className="h-4 w-4" />
-      </Link>
-      <Link
-        href="https://github.com/DylanChan23"
-        target="blank"
-        className="flex gap-1 items-center no-underline"
-      >
-        <span>Github</span>
-        <GithubIcon className="h-4 w-4" />
-      </Link>
-      <Link
-        href="mailto:dylanchan23@gmail.com"
-        className="flex gap-1 items-center no-underline"
-      >
-        <span>Email</span>
-        <MailFilledIcon className="h-4 w-4" />
-      </Link>
-    </header>
+    <footer className="fixed bottom-0 w-full z-20">
+      <div className="flex items-center justify-center gap-8 py-4 px-8 w-full bg-background/40">
+        <Link
+          href="https://www.linkedin.com/in/dylan-chan-a92b07271/"
+          target="blank"
+          className="flex gap-1 items-center no-underline"
+        >
+          <span>LinkedIn</span>
+          <LinkedinIcon className="h-4 w-4" />
+        </Link>
+        <Link
+          href="https://github.com/DylanChan23"
+          target="blank"
+          className="flex gap-1 items-center no-underline"
+        >
+          <span>Github</span>
+          <GithubIcon className="h-4 w-4" />
+        </Link>
+        <Link
+          href="mailto:dylanchan23@gmail.com"
+          className="flex gap-1 items-center no-underline"
+        >
+          <span>Email</span>
+          <MailFilledIcon className="h-4 w-4" />
+        </Link>
+      </div>
+    </footer>
   );
 }
